4231b2acf61a31ccf888e880ca94451006444906
Jiwon Han <jiwon1.han@gmail.com>
2020-02-07 09:57:12 -0800

    Create README.md

---
+++ README.md
@@ -0,0 +1,44 @@
+# Super Galactic Age Calculator
+
+#### An application that determines a user’s age based on a planet’s solar years. 2/7/20
+
+#### By **Jiwon Han**
+
+## Description
+
+A website created with object oriented JavaScript and HTML where a user input their age and program returns age depending on the planets.
+
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **Program returns user's age in Mercury years** | Topping options: "Cheese", "Pepperoni", "Vegetable" | Output: "Cheese" |
+| **Program returns user's age in Venus years**| Size options: "XL", "L", "M", "S" | Output: "M" |
+| **Program returns user's age in Mars years**| Input: "Cheese", "M" | Output: "$15" |
+| **Program returns user's age in Jupiter years**| Input: "Cheese", "M" | Output: "$15" |
+| **Program determines average life expectancy**| Input: "Cheese", "M" | Output: "$15" |
+
+## Setup/Installation Requirements
+
+1. Clone this repository.
+2. On your browser, navigate age planet calculator!
+
+## Known Bugs
+* No known bugs at this time.
+
+## Technologies Used
+* JavaScript
+* jQuery
+* Node.js
+* HTML
+* CSS
+
+## Support and contact details
+
+_Email jiwon1.han@gmail.com with any questions, comments, or concerns._
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2020 **_{Jiwon Han}_**
---
+++ __tests__/calculator.test.js
@@ -0,0 +1,38 @@
+// import { DayOfWeek } from './../src/scripts.js';
+// import { exportSpecifier, exportDefaultDeclaration } from '@babel/types';
+//
+// describe('Calculator', () => {
+//
+//   test('should create class with elements - year, month, day', () => {
+//     let newDayOfWeek = new DayOfWeek();
+//     expect(newDayOfWeek instanceof DayOfWeek).toEqual(true);
+//
+//   });
+//
+//   test('should create class with elements', () => {
+//     let newDayOfWeek = new DayOfWeek(2020, 2, 11);
+//     expect(newDayOfWeek.year).toEqual(2020);
+//     expect(newDayOfWeek.month).toEqual(2);
+//     expect(newDayOfWeek.day).toEqual(11);
+//   });
+//
+//   test('should return a certain number that represents day', () => {
+//     let var1 = new DayOfWeek(2020, 2, 5);
+//     let var2 = new DayOfWeek(2020, 3, 1);
+//     let var3 = new DayOfWeek(2020, 4, 13);
+//     expect(var1.calculateDate()).toEqual(3);
+//     expect(var2.calculateDate()).toEqual(0);
+//     expect(var3.calculateDate()).toEqual(1);
+//   });
+//
+//   test('should return string "This is not valid date to the invalid date' , () => {
+//     let var1 = new DayOfWeek("02020", 11, 6);
+//     let var2 = new DayOfWeek(2020, 13, 6);
+//     let var3 = new DayOfWeek(2020, 11, 33);
+//     let var4 = new DayOfWeek(2020, 3, "07")
+//     expect(var1.calculateDate()).toEqual("This is not valid date");
+//     expect(var2.calculateDate()).toEqual("This is not valid date");
+//     expect(var3.calculateDate()).toEqual("This is not valid date");
+//     expect(var4.calculateDate()).toEqual("This is not valid date");
+//   });
+// });
