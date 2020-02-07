b859a66dc6ec69be3d8f0a1522d1de7106ee180e
Jiwon Han <jiwon1.han@gmail.com>
2020-02-07 12:47:20 -0800

    Create test case1 regarding objects

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,38 +1,43 @@
-// import { DayOfWeek } from './../src/scripts.js';
-// import { exportSpecifier, exportDefaultDeclaration } from '@babel/types';
-//
-// describe('Calculator', () => {
-//
-//   test('should create class with elements - year, month, day', () => {
-//     let newDayOfWeek = new DayOfWeek();
-//     expect(newDayOfWeek instanceof DayOfWeek).toEqual(true);
-//
-//   });
-//
-//   test('should create class with elements', () => {
-//     let newDayOfWeek = new DayOfWeek(2020, 2, 11);
-//     expect(newDayOfWeek.year).toEqual(2020);
-//     expect(newDayOfWeek.month).toEqual(2);
-//     expect(newDayOfWeek.day).toEqual(11);
-//   });
-//
-//   test('should return a certain number that represents day', () => {
-//     let var1 = new DayOfWeek(2020, 2, 5);
-//     let var2 = new DayOfWeek(2020, 3, 1);
-//     let var3 = new DayOfWeek(2020, 4, 13);
-//     expect(var1.calculateDate()).toEqual(3);
-//     expect(var2.calculateDate()).toEqual(0);
-//     expect(var3.calculateDate()).toEqual(1);
-//   });
-//
-//   test('should return string "This is not valid date to the invalid date' , () => {
-//     let var1 = new DayOfWeek("02020", 11, 6);
-//     let var2 = new DayOfWeek(2020, 13, 6);
-//     let var3 = new DayOfWeek(2020, 11, 33);
-//     let var4 = new DayOfWeek(2020, 3, "07")
-//     expect(var1.calculateDate()).toEqual("This is not valid date");
-//     expect(var2.calculateDate()).toEqual("This is not valid date");
-//     expect(var3.calculateDate()).toEqual("This is not valid date");
-//     expect(var4.calculateDate()).toEqual("This is not valid date");
-//   });
-// });
+import { Planet } from './../src/scripts.js';
+import { exportSpecifier, exportDefaultDeclaration } from '@babel/types';
+
+describe('Planet', () => {
+
+  test('return planet objects', () => {
+    const earthYear = 365
+    let mercuryYear = new Planet('mercury', earthYear * 0.24);
+    let venusYear = new Planet('venus', earthYear * 0.62);
+    let marsYear = new Planet('mars', earthYear * 1.88);      let jupiterYear = new Planet('jupiter', earthYear * 11.86);
+    expect(mercuryYear).toEqual({"name": "mercury", "year": 87.6});
+    expect(venusYear).toEqual({"name": "venus", "year": 226.3});
+    expect(marsYear).toEqual({"name": "mars", "year": 686.1999999999999});
+    expect(jupiterYear).toEqual({"name": "jupiter", "year": 4328.9});
+  });
+});
+  //   test('should create class with elements', () => {
+  //     let newDayOfWeek = new DayOfWeek(2020, 2, 11);
+  //     expect(newDayOfWeek.year).toEqual(2020);
+  //     expect(newDayOfWeek.month).toEqual(2);
+  //     expect(newDayOfWeek.day).toEqual(11);
+  //   });
+  //
+  //   test('should return a certain number that represents day', () => {
+  //     let var1 = new DayOfWeek(2020, 2, 5);
+  //     let var2 = new DayOfWeek(2020, 3, 1);
+  //     let var3 = new DayOfWeek(2020, 4, 13);
+  //     expect(var1.calculateDate()).toEqual(3);
+  //     expect(var2.calculateDate()).toEqual(0);
+  //     expect(var3.calculateDate()).toEqual(1);
+  //   });
+  //
+  //   test('should return string "This is not valid date to the invalid date' , () => {
+  //     let var1 = new DayOfWeek("02020", 11, 6);
+  //     let var2 = new DayOfWeek(2020, 13, 6);
+  //     let var3 = new DayOfWeek(2020, 11, 33);
+  //     let var4 = new DayOfWeek(2020, 3, "07")
+  //     expect(var1.calculateDate()).toEqual("This is not valid date");
+  //     expect(var2.calculateDate()).toEqual("This is not valid date");
+  //     expect(var3.calculateDate()).toEqual("This is not valid date");
+  //     expect(var4.calculateDate()).toEqual("This is not valid date");
+  //   });
+  // });
--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-PlanetAge {
+class PlanetAge {
   constructor() {
     this.planets = [];
     this.planetId = 0;
@@ -13,4 +13,37 @@ PlanetAge {
   }
 }
 
-const earthYear = 365
+export class Planet {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+
+  planetAge(planets) {
+    const earthYear = 365
+    let mercuryYear = new Planet ('mercury', earthYear * 0.24);
+    let venusYear = new Planet('venus', earthYear * 0.62);
+    let marsYear = new Planet('mars', earthYear * 1.88);
+    let jupiterYear = new Planet('jupiter', earthYear * 11.86);
+
+    let myYear = 1991
+    const thisYear = 2020
+
+    let cal = (thisYear - myYear) * 365
+
+    if (mercuryYear) {
+      console.log (cal * mercuryYear.year)
+    } else if (venusYear) {
+      console.log (cal * venusYear.year)
+    } else if (marsYear) {
+      console.log (cal * marsYear.year)
+    } else if (jupiterYear) {
+      console.log (cal * jupiterYear.year)
+    }
+  }
+
+
+}
+
+
+const lifeExpentancy = 88
