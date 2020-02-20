c2f0eeb700de7a6eafb7a8d60b7d1111096feca2
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-02-19 21:18:17 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,50 +1,52 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import $ from 'jquery';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
+import $ from 'jquery'
 
-import { Planet } from './scripts';
-import { PlanetArray } from './scripts';
+import { Planet } from './scripts'
+import { PlanetArray } from './scripts'
 
 
 const earthYear = 365
-let mercury = new Planet('mercury', earthYear * 0.24);
-let venus = new Planet('venus', earthYear * 0.62);
-let mars = new Planet('mars', earthYear * 1.88);
-let jupiter = new Planet('jupiter', earthYear * 11.86);
+let mercury = new Planet('mercury', earthYear * 0.24)
+let venus = new Planet('venus', earthYear * 0.62)
+let mars = new Planet('mars', earthYear * 1.88)
+let jupiter = new Planet('jupiter', earthYear * 11.86)
 
 var planetArray = new PlanetArray
-planetArray.addPlanet(mercury);
-planetArray.addPlanet(venus);
-planetArray.addPlanet(mars);
-planetArray.addPlanet(jupiter);
+planetArray.addPlanet(mercury)
+planetArray.addPlanet(venus)
+planetArray.addPlanet(mars)
+planetArray.addPlanet(jupiter)
 
 
 $(document).ready(function(){
   
   $("#mercury").click(function(event){
-    event.preventDefault();
-    console.log('hihi')
-    let bornYear = document.getElementById('userBornYear').value;
-    let mercuryYear = planetArray.planets[0].planetAge(mercury, bornYear);
+    event.preventDefault()
+    let bornYear = document.getElementById('userBornYear').value
+    let mercuryYear = planetArray.planets[0].planetAge(mercury, bornYear)
     $("#mercuryResult").text(mercuryYear)
-  });
+  })
+  
   $("#venus").click(function(event){
-    event.preventDefault();
-    var bornYear = document.getElementById('userBornYear').value;
-    let venusYear = planetArray.planets[1].planetAge(venus, bornYear);
+    event.preventDefault()
+    var bornYear = document.getElementById('userBornYear').value
+    let venusYear = planetArray.planets[1].planetAge(venus, bornYear)
     $("#venusResult").text(venusYear)
-  });
+  })
+  
   $("#mars").click(function(event){
-    event.preventDefault();
-    var bornYear = document.getElementById('userBornYear').value;
-    let marsYear = planetArray.planets[2].planetAge(mars, bornYear);
-    $("#marsResult").text(marsYear);
+    event.preventDefault()
+    var bornYear = document.getElementById('userBornYear').value
+    let marsYear = planetArray.planets[2].planetAge(mars, bornYear)
+    $("#marsResult").text(marsYear)
   })
+  
   $("#jupiter").click(function(event){
-    event.preventDefault();
-    var bornYear = document.getElementById('userBornYear').value;
-    let jupiterYear = planetArray.planets[3].planetAge(jupiter, bornYear);
-    $("#jupiterResult").text(jupiterYear);
+    event.preventDefault()
+    var bornYear = document.getElementById('userBornYear').value
+    let jupiterYear = planetArray.planets[3].planetAge(jupiter, bornYear)
+    $("#jupiterResult").text(jupiterYear)
   })
-});
+})
